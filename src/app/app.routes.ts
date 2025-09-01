246bb0ecb74e651d14fecd10174fd13527b7e31f
negin-khademi <negin.khademi@gmail.com>
2025-09-01 13:37:37 +0330

    temp work

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { App } from "./app";
+import { Ui } from "./ui/ui";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: App },
+  { path: "ui", component: Ui },
+];
